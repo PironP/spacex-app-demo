18613563dd7f6025d94743591fc5c9751fdc69aa
Quentin Brunet <hello@quentinbrunet.com>
2019-07-25 08:48:35 +0200

    Improve payload and launch details pages

--- src/app/launches/pages/launch-details/launch-details.page.ts
+++ src/app/launches/pages/launch-details/launch-details.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Launch } from '../../models/launch.model';
 import * as moment from 'moment';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-launch-details',
@@ -13,16 +14,25 @@ export class LaunchDetailsPage implements OnInit, AfterViewInit {
   launch: Launch;
   missionName: string;
   timeToLaunch: string;
+  trustedVideoUrl: SafeResourceUrl;
 
   constructor(
     private route: ActivatedRoute,
-    private location: Location) {
+    private location: Location,
+    private domSanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
     this.launch =  this.route.snapshot.data.launch;
     if (this.launch.upcoming) {
       this.timeToLaunch = moment(this.launch.launch_date_local).fromNow();
+    }
+  }
+
+  ionViewWillEnter(): void {
+    if (this.launch.links.youtube_id) {
+      this.trustedVideoUrl = this.domSanitizer
+        .bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.launch.links.youtube_id);
     }
   }
 
